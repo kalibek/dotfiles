a6a5b8566956df0f266b4b69a6e81555b4399172
kalibek <kalibek.turgumbaev@gmail.com>
2021-01-22 10:51:40 +0600

    changes from arch

--- .autolock.sh
+++ .autolock.sh
@@ -1,10 +1,14 @@
 #!/bin/sh -e
 
+# switch keyboard to us
+setxkbmap us
+
 # Take a screenshot
-scrot /tmp/screen_locked.png
+scrot -o /tmp/screen_locked.png
 
 # Pixellate it 10x
 mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+#convert /tmp/screen_locked.png -blur 0x8 /tmp/screen_locked.png
 
 # Lock screen displaying this image.
 i3lock -i /tmp/screen_locked.png
